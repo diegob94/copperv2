1d1e6fb4504d0d198a41e51436f7c06ec788683b
diegob94 <dbim.bena@gmail.com>
2022-02-05 12:33:39 -0300

    Verilator running

--- sim/verilator_tb/sim_main.cpp
+++ sim/verilator_tb/sim_main.cpp
@@ -1,12 +1,20 @@
 #include <verilated.h>
 #include "Vcopperv.h"
 
+vluint64_t main_time = 0;       // Current simulation time
+// This is a 64-bit integer to reduce wrap over issues and
+// allow modulus.  This is in units of the timeprecision
+// used in Verilog (or from --timescale-override)
+
 int main(int argc, char** argv, char** env) {
-    VerilatedContext* contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    Vcopperv* top = new Vcopperv{contextp};
-    while (!contextp->gotFinish()) { top->eval(); }
-    delete top;
-    delete contextp;
-    return 0;
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vcopperv* top = new Vcopperv{contextp};
+  while (!contextp->gotFinish()) { 
+    top->eval();
+		main_time++;
+  }
+  delete top;
+  delete contextp;
+  return 0;
 }
