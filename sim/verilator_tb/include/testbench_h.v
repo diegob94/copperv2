e58b4bf12bed8f32125f10b84396ee226048d59a
diegob94 <dbim.bena@gmail.com>
2022-02-06 03:21:21 -0300

    Adding testbench

---
+++ sim/verilator_tb/include/testbench_h.v
@@ -0,0 +1,6 @@
+`define STRING              reg [1023:0]
+`define PERIOD              10
+`define TRUE                1
+`define FALSE               0
+`define CPU_INST            tb.dut
+`define FAKE_MEM_ADDR_WIDTH 24
