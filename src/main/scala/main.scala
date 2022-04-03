4f3d2d318accff5ab3d58c506f61068ec10cdc85
diegob94 <dbim.bena@gmail.com>
2022-04-03 12:39:11 -0400

    Remove wishbone code and use external repo

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,5 +7,4 @@ object Copperv2Driver extends App {
 //  (new ChiselStage).execute(Array("--emit-modules", "verilog"),Seq(ChiselGeneratorAnnotation(() => new copperv2.copperv2)))
   (new ChiselStage).emitVerilog(new wishbone.WishboneAdapter(32,32,1), verilog_args ++ Array("-o","wb_adapter.v"))
 //  (new ChiselStage).emitVerilog(new copperv2.CoppervSoC, verilog_args ++ Array("-o","copperv_soc_top.v"))
-  (new ChiselStage).emitVerilog(new wishbone.CrossBar(2), verilog_args ++ Array("-o","crossbar.v"))
 }
